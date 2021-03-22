97656b6b324a204b41dcf8c0f0dc2a27c14ef894
yolsha <inx.yolsha@gmail.com>
2021-03-23 01:56:17 +0300

    Update Tabs.vue

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div v-bind="sortedArray(users)">
     <UserID
-      v-for="user in sortedArray(users)"
+      v-for="user in users"
       :key="user.point"
       :user="user"
     />
@@ -13,6 +13,9 @@ import UserID from "./UserID";
 
 export default {
   name: "Tabs",
+  props: {
+    msg: String
+  },
   components: {
     UserID
   },
@@ -21,31 +24,31 @@ export default {
       users: [
         {
           point: 56,
-          numbe: '',
+          id: 1,
           name: "Adrian Schubert",
           avatar: "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
         },
         {
           point: 66,
-          numbe: '',
+          id: 2,
           name: "Violet Gates",
           avatar: "https://pickaface.net/gallery/avatar/freud51c8b3f65e7dc.png"
         },
         {
           point: 46,
-          numbe: '',
+          id: 3,
           name: "Steve Jobs",
           avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
         },
         {
           point: 54,
-          numbe: '',
+          id: 4,
           name: "Yassine Smith",
           avatar: "https://pickaface.net/gallery/avatar/unr_yassine_191124_2012_3gngr.png"
         },
         { 
           point: 50,
-          numbe: '',
+          id: 5,
           name: "Senior Saez",
           avatar: "https://pickaface.net/gallery/avatar/elmedinilla541c03412955c.png"
         }
@@ -54,18 +57,22 @@ export default {
   },
   computed: {
   sortedArray: function() {
-    var user0 = this.users;
-    console.log(user0);
-
     function compare(a, b) {
       if (a.point > b.point){
         return -1;
       }
-      if (a.point > b.point)
+      if (a.point < b.point)
         return 1;
-        return 0;
       }
-    return user0.sort(compare);
+      var user0 = this.users;
+      console.log(user0);
+      user0.sort(compare);
+
+      for(var i=0; i < user0.length; i++ ){
+        user0[i].id = (i+1);
+      };
+      
+      return user0;
     }
   }
 }
