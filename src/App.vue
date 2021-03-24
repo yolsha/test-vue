7f922fa7918f4e0da5538e7289e21b9666b29c72
yolsha <yolsha@github.com>
2021-03-24 08:41:59 +0300

    last222

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-
     <DataTabs />
-
-    <h1> _____________________________</h1>
-
-    <Tabs />
-
   </div>
 </template>
 
 <script>
-import Tabs from "./components/Tabs";
 import DataTabs from "./components/DataTabs";
 
 export default {
   name: "App",
   components: {
-    Tabs, DataTabs
+    DataTabs
   }
 };
 </script>
